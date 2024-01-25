2a1b2eee13630ddf9cc4b4f8898e6a4476779e73
toEpam <toepammiddle@gmail.com>
2024-01-25 21:58:51 +0500

    .

--- bot.py
+++ bot.py
@@ -3,51 +3,39 @@ import logging
 import sys
 from os import getenv
 
-from aiogram import Bot, Dispatcher, types
+import wikipedia
+from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
+from icecream import ic
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN")
+TOKEN = '1764547518:AAHvEJz9nGEeBIzPmvXpcqmmGrkKI5E_vmg'
+wikipedia.set_lang('uz')
 
-# All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
 
-@dp.message(CommandStart(), Command("help"))
+@dp.message(CommandStart(), Command('help'))
 async def command_start_handler(message: Message) -> None:
     """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+        This handler will be called when user sends `/start` or `/help` command
+        """
+    await message.reply("Wikipeida Botiga Xush Kelibsiz!")
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
+    ic(message.chat.id)
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid maqola topilmadi")
 
 async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
