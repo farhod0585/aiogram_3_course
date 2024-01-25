16047549d199f4cbb5a51a382d83cea0b8ba308d
toEpam <toepammiddle@gmail.com>
2024-01-25 22:36:28 +0500

    .

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
 import asyncio
 import logging
 import sys
-from os import getenv
 
-import wikipedia
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold
 from icecream import ic
 
 TOKEN = '1764547518:AAHvEJz9nGEeBIzPmvXpcqmmGrkKI5E_vmg'
-wikipedia.set_lang('uz')
+from oxfordLookup import getDefinitions
+from googletrans import Translator
+
+translator = Translator()
 
 dp = Dispatcher()
 
@@ -22,17 +22,33 @@ async def command_start_handler(message: Message) -> None:
     """
         This handler will be called when user sends `/start` or `/help` command
         """
-    await message.reply("Wikipeida Botiga Xush Kelibsiz!")
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    ic(message.chat.id)
     try:
-        respond = wikipedia.summary(message.text)
-        await message.answer(respond)
+        ic(message)
+        lang = translator.detect(message.text).lang
+        if len(message.text.split()) > 2:
+            dest = 'uz' if lang == 'en' else 'en'
+            await message.reply(translator.translate(message.text, dest).text)
+        else:
+            if lang == 'en':
+                word_id = message.text
+            else:
+                word_id = translator.translate(message.text, dest='en').text
+
+            lookup = getDefinitions(word_id)
+            if lookup:
+                await message.reply(f"Word: {word_id} \nDefinitions:\n{lookup['definitions']}")
+                if lookup.get('audio'):
+                    await message.reply_voice(lookup['audio'])
+            else:
+                await message.reply("Bunday so'z topilmadi")
     except:
-        await message.answer("Bu mavzuga oid maqola topilmadi")
+        await message.answer("Xatolik yuz berdi")
+
 
 async def main() -> None:
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
